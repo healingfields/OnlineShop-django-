74dd0e5ec509f2364b05daf4b971a67145b5d336
healingfields <dihratene@gmail.com>
2022-01-04 03:31:33 +0100

    created the product list template

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,7 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
   category = None
   categories = Category.objects.all()
-  products = Product.objects.all(available=True)
+  products = Product.objects.filter(available=True)
   if category_slug:
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
