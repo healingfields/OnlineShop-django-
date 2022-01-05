5cb81fc0a9c8f801206d30f36877b29e33bc5866
healingfields <dihratene@gmail.com>
2022-01-05 19:08:21 +0100

    registering the order in the administration site

--- orders/models.py
+++ orders/models.py
@@ -21,19 +21,19 @@ class Order(models.Model):
   def get_total_cost(self):
     return sum(item.get_cost() for item in self.items.all())
   
-  class OrderItem(models.Model):
-    product = models.ForeignKey(Product,
-                                related_name='order_items',
-                                on_delete=models.CASCADE)
-    order = models.ForeignKey('Order',
-                              related_name='items',
+class OrderItem(models.Model):
+  product = models.ForeignKey(Product,
+                              related_name='order_items',
                               on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+  order = models.ForeignKey('Order',
+                            related_name='items',
+                            on_delete=models.CASCADE)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  quantity = models.PositiveIntegerField(default=1)
 
-    def __str__(self):
-      return str(self.id)
+  def __str__(self):
+    return str(self.id)
 
-    def get_cost(self):
-      return self.price * self.quantity
+  def get_cost(self):
+    return self.price * self.quantity
 
