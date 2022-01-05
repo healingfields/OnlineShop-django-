8e5741e61839e5a843963a8eba70e7432f2ed430
healingfields <dihratene@gmail.com>
2022-01-05 01:41:18 +0100

    fixed an error in the cart class

--- cart/cart.py
+++ cart/cart.py
@@ -4,7 +4,7 @@ from shop.models import Product
 
 class Cart(object):
   
-  def __inti__(self, request):
+  def __init__(self, request):
     self.session = request.session
     cart = self.session.get(settings.CART_SESSION_ID)
     if not cart:
@@ -14,14 +14,14 @@ class Cart(object):
   def save(self):
     self.session.modified = True
 
-  def add(self, product, quantity=1, overrid_quantity=False):
+  def add(self, product, quantity=1, override_quantity=False):
     product_id = str(product.id)
     if product_id not in self.cart:
-      self.cart['product_id']={'quantity':0, 'price':str(product.price)}
-    if overrid_quantity:
-      self.cart['product_id']['quantity'] = quantity
+      self.cart[product_id]={'quantity':0, 'price':str(product.price)}
+    if override_quantity:
+      self.cart[product_id]['quantity'] = quantity
     else:
-      self.cart['product_id']['quantity'] += quantity
+      self.cart[product_id]['quantity'] += quantity
     self.save()
    
   def remove(self, product):
